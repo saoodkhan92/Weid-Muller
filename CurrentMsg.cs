3cb0673d028d5bc4c5e2117f48af578ae4e37a6a
saoodkhan92 <68372892+saoodkhan92@users.noreply.github.com>
2020-07-16 13:19:35 +0400

    Create CurrentMsg.cs

---
+++ CurrentMsg.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WeiderMuller_Test_Utility
+{
+    public partial class CurrentMsg : Form
+    {
+        public CurrentMsg()
+        {
+            InitializeComponent();
+        }
+        bool blclose = true;
+        bool blFormClosing = false;
+
+        private void CurrentMsg_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Bitmap.FromHicon(SystemIcons.Warning.Handle);
+            blFormClosing = false;
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            blclose = true;
+            if (blclose == true)
+            {
+                blclose = true;
+                blFormClosing = true;
+                this.Close();
+            }
+            else
+                blclose = false;
+            ((System.Windows.Forms.Form)((System.Windows.Forms.ButtonBase)sender).Parent).DialogResult = DialogResult.OK;
+
+
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            ((System.Windows.Forms.Form)((System.Windows.Forms.ButtonBase)sender).Parent).DialogResult = DialogResult.Cancel;
+            //Application.Exit();
+        }
+
+       
+
+        private void CurrentMsg_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            blclose = false;
+            //if (blclose == false)
+            //   blFormClosing = true;
+
+            if (blFormClosing == false)
+            {
+                ((System.Windows.Forms.Form)sender).DialogResult = DialogResult.Cancel;
+
+            }
+        }
+
+        public void PassMessage(string Msg)
+        {
+            label1.Text = Msg;
+        }       
+    }
+}
